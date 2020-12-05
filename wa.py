ba7b24b924412ff121ace27d657d97d1803bf64a
javier <jgalvarez.jga@gmail.com>
2020-12-05 12:04:37 -0800

    xpath_v1

--- wa.py
+++ wa.py
@@ -13,6 +13,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
 #Hay que cerrar las conexiones del whatsApp con el navegador
 print("Abriendo el Navegador ...")
 driver = webdriver.Firefox()
@@ -25,16 +28,18 @@ while name != "salir":
 	if name == "salir":
 	    sys.exit()
 	else:
-	#Nombre del Chat
+	    #Nombre del Chat
 		user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 		user.click()
-		#Texto
-		msg_box = driver.find_element_by_class_name("_3u328.copyable-text.selectable-text")
+		#Textobox que define la class CSS JS de WhatsApp
+		msg_box = driver.find_element_by_class_name("_1awRl.copyable-text.selectable-text")
 		msg_box.send_keys(msg)
-		#Es un button
-		driver.find_element_by_class_name("_3M-N-").click()
-
-	
+		#Button de envío        
+        driver.find_element_by_xpath("//span[@data-icon='send']")
+        element = driver.find_element_by_xpath("//form[input/@name ='search']") 
+        #boton_enviar = driver.find_element_by_xpath('//span[@data-icon='send']')
+		#driver.find_element_by_class_name("_3uMse").click()        
+        boton_enviar.click()
 
 #Salir del navegador
 #driver.quit()
